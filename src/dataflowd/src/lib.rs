4f018da60c776d58ec8cdd2d6e558faa86a8dfbf
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2022-03-11 14:10:15 +0100

    dataflowd: add option to enable persist runtime

--- src/dataflowd/src/lib.rs
+++ src/dataflowd/src/lib.rs
@@ -8,3 +8,32 @@
 // by the Apache License, Version 2.0.
 
 //! Independent dataflow server support.
+
+use compile_time_run::run_command_str;
+
+use mz_build_info::BuildInfo;
+
+#[allow(missing_docs)]
+pub const BUILD_INFO: BuildInfo = BuildInfo {
+    version: env!("CARGO_PKG_VERSION"),
+    sha: run_command_str!(
+        "sh",
+        "-c",
+        r#"if [ -n "$MZ_DEV_BUILD_SHA" ]; then
+            echo "$MZ_DEV_BUILD_SHA"
+        else
+            # Unfortunately we need to suppress error messages from `git`, as
+            # run_command_str will display no error message at all if we print
+            # more than one line of output to stderr.
+            git rev-parse --verify HEAD 2>/dev/null || {
+                printf "error: unable to determine Git SHA; " >&2
+                printf "either build from working Git clone " >&2
+                printf "(see https://materialize.com/docs/install/#build-from-source), " >&2
+                printf "or specify SHA manually by setting MZ_DEV_BUILD_SHA environment variable" >&2
+                exit 1
+            }
+        fi"#
+    ),
+    time: run_command_str!("date", "-u", "+%Y-%m-%dT%H:%M:%SZ"),
+    target_triple: env!("TARGET_TRIPLE"),
+};
